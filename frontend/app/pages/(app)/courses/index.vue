5b1d1a303c5fdd1445291ff4ec15e65c0dfee0dd
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-29 18:35:16 +0200

    brand colors

--- frontend/app/pages/(app)/courses/index.vue
+++ frontend/app/pages/(app)/courses/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+</script>
+
 <template>
 	<p>courses</p>
+	<Button class="w-fit">Primary</Button>
+	<Button variant="secondary" class="w-fit">Secondary</Button>
 </template>
