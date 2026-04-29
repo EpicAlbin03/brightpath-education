87a1645de8fa21527194c01a604978ce74fb796d
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-29 14:42:55 +0200

    frontend init

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ArrowUpIcon } from "lucide-vue-next"
+import { Button } from "@/components/ui/button"
+import TestComponent from "~/components/TestComponent.vue"
+</script>
+
+<template>
+  <div class="flex flex-wrap items-center gap-2 md:flex-row">
+    <Button variant="outline"> Button </Button>
+    <Button variant="outline"> Button </Button>
+    <Button> Button </Button>
+    <Button variant="destructive"> Button </Button>
+    <Button variant="secondary"> Button </Button>
+    <Button variant="outline" size="icon" aria-label="Submit">
+      <ArrowUpIcon />
+    </Button>
+
+    <TestComponent />
+  </div>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,36 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  devServer: {
+    port: 5173,
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vueuse/core',
+      ]
+    },
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt', '@nuxt/eslint'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/lib/components/ui'
+  },
+})
