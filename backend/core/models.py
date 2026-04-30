640dafe0a9113f077b27e1bb23370e91e9f33f95
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-30 21:37:41 +0200

    uncomment created_at

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
 
 
 class Student(models.Model):
@@ -23,18 +21,18 @@ class Student(models.Model):
     #     null=True,
     #     blank=True,
     # )
-    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10, unique=True)
     description = models.TextField(blank=True)
 
-    
+
 
     def __str__(self):
         return f"{self.code} — {self.name}"
-    
+
