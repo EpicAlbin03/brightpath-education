93c040ddd465515aae64067536e81afc9896b9e4
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-02 18:11:15 +0200

    include

--- frontend/server/api/students/[id]/index.get.ts
+++ frontend/server/api/students/[id]/index.get.ts
@@ -1,18 +1,33 @@
 import { Student } from '~~/shared/types';
 import { getToken, validateIdParams } from '../../utils';
+import { z } from 'zod';
+
+const querySchema = z.object({
+	include: z.enum(['courses']).optional()
+});
 
 export default defineEventHandler(async (event) => {
 	const id = await validateIdParams(event);
+	const query = await getValidatedQuery(event, querySchema.safeParse);
+
+	if (!query.success) {
+		throw createError({ statusCode: 422, message: 'Invalid query parameters' });
+	}
+	const queryString = query.data.include ? `?include=${query.data.include}` : '';
+
 	const config = useRuntimeConfig();
 	const token = getToken(event);
 
 	try {
-		const student = await $fetch<Student>(`${config.public.apiBase}/students/${id}/`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-				Accept: 'application/json'
+		const student = await $fetch<Student>(
+			`${config.public.apiBase}/students/${id}/${queryString}`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+					Accept: 'application/json'
+				}
 			}
-		});
+		);
 
 		return student;
 	} catch (error) {
