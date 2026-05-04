50aeb995b35cd12dd1fc872ff2e1e429eab24df5
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-04 09:07:52 +0200

    rm unnecessary template

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 definePageMeta({
-  middleware: [
-    () => {
-      const accessToken = useCookie<string | null>('access_token')
-      if (accessToken.value) {
-        return navigateTo('/students', { replace: true })
-      }
-      return navigateTo('/login', { replace: true })
-    }
-  ]
-})
+	middleware: [
+		() => {
+			const accessToken = useCookie<string | null>('access_token');
+			if (accessToken.value) {
+				return navigateTo('/students', { replace: true });
+			}
+			return navigateTo('/login', { replace: true });
+		}
+	]
+});
 </script>
-
-<template>
-  <div />
-</template>
